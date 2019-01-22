8754774c75f58cbce66982963ea4cf2fa409653e
Julliana Bauer <bjulliana@gmx.com>
2019-01-21 20:11:05 -0500

    lint setup

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+	configureWebpack: {
+		plugins: [
+			new StyleLintPlugin({
+				configFile : '.stylelintrc',
+				context    : 'src/scss',
+				files      : '**/*.scss',
+				failOnError: false,
+				quiet      : false,
+				syntax     : 'scss',
+			}),
+		]
+	}
+};
