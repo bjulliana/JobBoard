638312cba040e6335fcb7fcbc343c10e7c4fe5ed
Julliana Bauer <bjulliana@gmx.com>
2019-02-02 21:20:23 -0500

    update project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    root         : true,
+    env          : {
+        node: true
+    },
+    'extends'    : [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules        : {
+        'generator-star-spacing'     : 'off',
+        'import/first'               : ['error', 'DISABLE-absolute-first'],
+        'indent'                     : [2, 4],
+        'comma-dangle'               : ['error', 'never'],
+        'key-spacing'                : ['error', {'align': 'colon'}],
+        'object-curly-spacing'       : ['error', 'never'],
+        'quote-props'                : ['error', 'as-needed'],
+        'max-len'                    : 0,
+        'semi'                       : ['error', 'always'],
+        'space-before-function-paren': ['error', {'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always'}],
+        'vue/require-v-for-key'      : 'off',
+        'no-console'                 : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger'                : process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+};
